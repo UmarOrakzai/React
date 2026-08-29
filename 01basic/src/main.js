88fa5f599a98b9ede01f52555fc36665e27f0a56
umarorakzai <umarorakzai315@gmail.com>
2026-08-29 03:49:08 -0700

    start again

--- 01basic/src/main.js
+++
@@ -1,7 +0,0 @@
-
-
-import User from './user.jsx'
-
-document.querySelector('#app').innerHTML = User()
-
-setupCounter(document.querySelector('#counter'))
--- 01basic/src/user.js
+++
@@ -1,8 +0,0 @@
-function User(){
-    return(
-        <h2>Hello Word</h2>
-    )
-}
-
-
-export default User
