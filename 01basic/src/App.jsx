d9e8d49d9e04692892e0a8f423fc8443d3a15311
umarorakzai <umarorakzai315@gmail.com>
2026-08-29 04:09:00 -0700

    Updated

--- 01basic/src/App.jsx
+++ 01basic/src/App.jsx
@@ -1,6 +1,9 @@
+import User from "./user"
+
 function App() {
   return (
     <div style={{textAlign: 'center', padding: '40px', fontFamily: 'Arial'}}>
+      <User/>
       <h1>The Fasion Spot</h1>
       <p>Kapre aur Cosmetics ki Shop - Hangu</p>
       <button style={{padding: '10px 20px'}}>Shop Now</button>
---
+++ 01basic/src/user.jsx
@@ -0,0 +1,7 @@
+function User(){
+    return(
+        <h1>User Name is Umar</h1>
+    )
+}
+
+export default User
