5917ee16c8cf1d8118ec3ba063cc0f18d5e60ec8
umarorakzai <umarorakzai315@gmail.com>
2026-08-29 04:04:10 -0700

    Creating project

---
+++ 01basic/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{textAlign: 'center', padding: '40px', fontFamily: 'Arial'}}>
+      <h1>The Fasion Spot</h1>
+      <p>Kapre aur Cosmetics ki Shop - Hangu</p>
+      <button style={{padding: '10px 20px'}}>Shop Now</button>
+    </div>
+  )
+}
+
+
+export default App
